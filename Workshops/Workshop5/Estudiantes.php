f5be8b72a01302b1a4f026b4aba783be797f171b
JosueSoto23 <deivysoto66@gmail.com>
2021-03-10 01:39:57 -0600

    Casi terminado worksop5

--- Workshops/Workshop5/Estudiantes.php
+++ Workshops/Workshop5/Estudiantes.php
@@ -1,20 +1,31 @@
 <?php
 
-    include "Auth.php"
+    $inc = include("Connection.php");
+    include "Auth.php";
 
-?>
+    for ($i = 1; $i <= count($nombres, COUNT_NORMAL); $i++) {
+        if("208100649" === $cedulas[$i-1]){
+            $cedula = $cedulas[$i-1];
+            $nombre = $nombres[$i-1];
+        }
+    }
+	?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title> Matrículas </title>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" type="text/css" href="Style/Style.css">
+
+    <title> Workshop 5 </title>
+
 </head>
 
-<body>
-
+<body>    
     <header>
         <div>
             <h2> Estudiantes </h2>
@@ -22,14 +33,15 @@
     </header>
 
     <div id="registradas"><br>
-        	<h2><?php echo $cedula; ?></h2>
-        	<div>
-        		<p>
-        			<b> Nombre: </b> <?php echo $nombre ?> <br>
-        		</p>
-        	</div>
-        </div> 
-
+        <h2><?php echo $cedula ?></h2>
+        <div>
+            <p>
+            	<b> Nombre: </b> <?php echo $nombre ?> <br>
+        	</p>
+        </div>
+    </div> 
 </body>
 
-</html>
+<?php
+	
+?>
