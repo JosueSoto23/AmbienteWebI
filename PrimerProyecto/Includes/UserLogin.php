af913c3d823d015e042d3453d879eeb4b1488731
JosueSoto23 <deivysoto66@gmail.com>
2021-03-12 15:07:42 -0600

    User Login Test1

--- PrimerProyecto/Includes/UserLogin.php
+++ PrimerProyecto/Includes/UserLogin.php
@@ -1,21 +1,35 @@
 <?php
 
-  require "Connection.php";
+include "Connection.php";
 
-  if($_POST) {
-    $email = $_REQUEST['email'];
-    $pass = $_REQUEST['pass'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+session_start();
+$_SESSION['user'] = $user;
 
-    $user = UserAuthentication($email, $pass);
 
-    if($user) {
-      session_start();
-      $_SESSION['user'] = $user;
+$conex = mysqli_connect("localhost", "root", "", "primerproyecto");
 
-      include "UserRegister.php";
-    } else {
-      header('Location: index.php?status=login');
-    }
-  }
+$consult = "SELECT * FROM users where email = '$email' and pass = '$pass'";
+$result = mysqli_query($conex, $consult);
 
+$filas = mysqli_num_rows($result);
+
+if ($filas) {
+
+  //header("location:home.php");
+  echo "ADIOS";
+
+} else {
 ?>
+  <?php
+  //include("index.html");
+  echo "HOLA";
+
+  ?>
+  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+<?php
+}
+
+mysqli_free_result($result);
+mysqli_close($conex);
--- PrimerProyecto/Index.php
+++ PrimerProyecto/Index.php
@@ -1,19 +1,3 @@
-<?php
-
-$message = "";
-if (!empty($_REQUEST['status'])) {
-  switch ($_REQUEST['status']) {
-    case 'login':
-      $message = 'User does not exist';
-      break;
-    case 'error':
-      $message = 'There was a problem inserting the user';
-      break;
-  }
-}
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,33 +21,25 @@ if (!empty($_REQUEST['status'])) {
     <h3 class="title"> Inicio de Sesión </h3>
   </header>
 
-  <section class="home">
     <div class="container">
-      <form action="POST">
+      <form action="POST" action="Includes/UserLogin.php" method="POST">
 
         <div class="input-group mb-3">
           <span class="input-group-text" id="basic-addon1"> Correo </span>
-          <input type="email" name="email" class="form-control" aria-label="Email" aria-describedby="basic-addon1">
+          <input type="email" name="email" class="form-control">
         </div>
 
         <div class="input-group mb-3">
           <span class="input-group-text" id="basic-addon1"> Contraseña </span>
-          <input type="password" name="pass" class="form-control" aria-label="Password" aria-describedby="basic-addon1">
+          <input type="password" name="pass" class="form-control">
         </div>
 
         <br>
-        <button type="submit" name="login" class="btn btn-primary"> Ingresar </button><br>
+        <button type="submit" name="login" value="Ingresar" class="btn btn-primary"> Ingresar </button><br>
         <a href="UserRegister.php"> Registrarse </a>
 
       </form>
     </div>
-  </section>
-
-  <?php
-
-  include "Includes/UserLogin.php";
-
-  ?>
 
   <footer>
     <div class="footer">
