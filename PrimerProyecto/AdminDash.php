570dc01484365e51aa8b801dfe5a2be6eb6ea721
JosueSoto23 <deivysoto66@gmail.com>
2021-03-13 01:23:49 -0600

    Dashboard advanced

--- PrimerProyecto/AdminDash.php
+++ PrimerProyecto/AdminDash.php
@@ -18,7 +18,7 @@ require 'Includes/UserLogin.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="Style/Style.css">
 
-    <title>Document</title>
+    <title> My News Cover </title>
 
 </head>
 
@@ -28,13 +28,15 @@ require 'Includes/UserLogin.php';
         <h1><a href="Includes/UserLogout.php"><img src="Images/logo.png"></a></h1>
         <button type="button" class="btn btn-dark"> <img class="icon" src="Images/user_50px.png" alt="x" /> <?php echo "Admin"; ?> </button>
         <button type="button" class="btn btn-light" onclick="location.href='Includes/UserLogout.php'"> Logout </button>
-        <button type="button" class="btn btn-light" onclick="location.href='NewsSources.php'"> News Sources </button>
-        <h3 class="title"> Your Unique News Cover </h3>
+        <button type="button" class="btn btn-light" onclick="location.href='AdminDash.php'"> Categories </button>
+        <h3 class="title"> Categories </h3>
         <hr class="hr-title">
     </header>
 
-    <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos maxime minima nisi esse commodi ullam vero non, at quis quasi dicta rerum necessitatibus, temporibus, sequi labore beatae optio est.</p>
+    <div class="container">
+        
+
+        <button onclick="location.href='AddCategories.php'" name="open-add-categories" class="btn btn-primary"> Add New </button><br>
     </div>
 
     <footer>
