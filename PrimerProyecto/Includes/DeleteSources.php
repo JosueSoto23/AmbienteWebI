ff2264948db10c3978310787b97d9af925e056bc
JosueSoto23 <deivysoto66@gmail.com>
2021-03-15 16:43:57 -0600

    CRUD News done

--- PrimerProyecto/Includes/DeleteSources.php
+++ PrimerProyecto/Includes/DeleteSources.php
@@ -2,11 +2,19 @@
 include "Connection.php";
 
 deleteSources($_GET['id']);
+deleteNews($_GET['id']);
 
 function deleteSources($id)
 {
     $conex = getConnection();
     $consult = "DELETE FROM newssources WHERE id = '$id'";
+    $query = mysqli_query($conex, $consult);
+}
+
+function deleteNews($id)
+{
+    $conex = getConnection();
+    $consult = "DELETE FROM news WHERE news_source_id = '$id'";
     $query = mysqli_query($conex, $consult);
 
 }
